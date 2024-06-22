80c7843a6e5d658bd68c86bd7aa01f4f725a4f5a
league <yayalisa91@gmail.com>
2024-06-22 14:26:52 -0700

    01 intro to stack

--- src/_00_IntroToStacks/_01_IntroToStack.java
+++ src/_00_IntroToStacks/_01_IntroToStack.java
@@ -9,14 +9,35 @@ public class _01_IntroToStack {
     public static void main(String[] args) {
         // 1. Create a Stack of Doubles
         //    Don't forget to import the Stack class
-
+Stack<Double> duble = new Stack<Double>();
         // 2. Use a loop to push 100 random doubles between 0 and 100 to the Stack.
 
+for (int i = 0; i < 100; i++) {
+	Random rand = new Random();
+	duble.push(rand.nextDouble()*100);
+}
+
+
+
         // 3. Ask the user to enter in two numbers between 0 and 100, inclusive. 
+String num1 = JOptionPane.showInputDialog("Please enter a number between 0 and 100");
+String num2 = JOptionPane.showInputDialog("Please enter another number between 0 and 100");
+int one = Integer.parseInt(num1);
+int two = Integer.parseInt(num2);
+if(two < one) {
+	int holder = two;
+	two = one;
+	one = holder;
+}
 
         // 4. Pop all the elements off of the Stack. Every time a double is popped that is
         //    between the two numbers entered by the user, print it to the screen.
-
+for (int i = 0; i < duble.size(); i++) {
+	Double nice = duble.pop();
+	if(nice > one && nice < two) {
+		System.out.println(nice); 
+	}
+}
 
         // EXAMPLE:
         // NUM 1: 65
